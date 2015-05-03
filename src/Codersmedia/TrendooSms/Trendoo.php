3af2500c8b08f22e3bc87dcadf0971b620340720
Matteo Schiona <schiona.matteo@gmail.com>
2015-05-03 04:04:11 +0000

    Conversion to curl

--- src/Codersmedia/TrendooSms/Trendoo.php
+++ src/Codersmedia/TrendooSms/Trendoo.php
@@ -19,12 +19,6 @@ class Trendoo {
 	protected $password;
 
 	/*
-     * HTTP REQUEST
-     */
-	protected $client;
-	protected $request;
-
-	/*
      * DON'T TOUCH THIS PARAM!!!!!!!!
      * Referer to: http://www.trendoo.it/pdf/API_http.pdf
      */
@@ -36,7 +30,7 @@ class Trendoo {
 	protected $responseValid    = 'OK';
 	protected $iso              = 'IT';    //ISO 3166
 	protected $charset          = 'UTF-8';
-	protected $base_url          = 'https://api.trendoo.it/Trend/';
+	protected $base_url          = "https://api.trendoo.it/Trend/";
 	protected $send_endpoint    = 'SENDSMS';
 
 	public function __construct()
@@ -46,29 +40,77 @@ class Trendoo {
 		$this->debug      = Config::get("trendoo.debug");
 	}
 
-	protected function buildRequest(Array $params = null){
+	protected function injectLoginParams(){
+		return [
+			'login' => $this->username,
+			'password' => $this->password
+		];
+	}
+	protected function doRequest($endpoint, Array $args = null){
 
-		$this->client = new Client([
-			'base_url' => ['{base_url}{request_url}',
-				[
-					'base_url' => $this->base_url,
-					'request_url' => $this->request
-				]
-			],
-			'defaults' => [
-				'query'   => [
-					'login' => $this->username,
-					'password' => $this->password,
-					$params
-				],
-			]
-		]);
+		$loginParams = $this->injectLoginParams();
+
+		/* Script URL */
+		$url = $this->base_url . $endpoint;
+
+		/* $_GET Parameters to Send */
+		$params = ($args != null) ? array_merge($loginParams,$args) : $loginParams;
+
+		/* Update URL to container Query String of Paramaters */
+		$url .= '?' . http_build_query($params);
+
+		/* cURL Resource */
+		$ch = curl_init();
+
+		/* Set URL */
+		curl_setopt($ch, CURLOPT_URL, $url);
+
+		/* Tell cURL to return the output */
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		/* Tell cURL NOT to return the headers */
+		curl_setopt($ch, CURLOPT_HEADER, false);
+
+		/* Execute cURL, Return Data */
+		$this->responseData = curl_exec($ch);
+
+		/* Check HTTP Code */
+		$this->responseStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		/* Close cURL Resource */
+		curl_close($ch);
+
+	}
+	protected function parseResponse($responseBody){
+		$response = explode($this->responseDivider, $responseBody);
+		if($response[0] == $this->responseError){
+			return $this->responseWithError($response[1],$response[2]);
+		}
+		else {
+			return $this->responseWithSuccess($response[1],$response[2]);
+		}
+	}
+
+	protected function responseWithError($code,$message) {
+		return response()->json(["success" => false, "data" => ['error' => $code, 'message' => urldecode($message)]]);
+	}
+
+	protected function responseWithSuccess($data) {
+		return response()->json(["success" => false, "data" => [$data]]);
 	}
 
 	public function connect(){
-		$this->buildRequest(null);
-		echo 'prova';
-		dd($this->client);
+		try {
+
+			$this->doRequest($this->send_endpoint);
+
+			if($this->responseStatus == 200) {
+				return $this->parseResponse($this->responseData);
+			}
+			else { return 'General error'; }
+		} catch (Exception $e) {
+
+		}
 	}
 
 	public function sendMessage($to, $message)
